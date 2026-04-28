04bc7cf99b427e0a9216d5c759e8e04511e63d4b
xumengyao <qiutian132512@gmail.com>
2026-04-28 15:14:45 +0800

    make cache path more robust

--- python/sglang/srt/mem_cache/fusionrag_cache.py
+++ python/sglang/srt/mem_cache/fusionrag_cache.py
@@ -231,13 +231,13 @@ class FusionragCache(RadixCache):
 
         cache_path_root = "/mnt/data3"
         served_model_name = server_args.served_model_name
-        # if not os.path.exists(cache_path_root):
-        #     cache_path_root = "/mnt/data"
-        self.cache_path = f"/mnt/data3/xmy/fusionrag_tree_cache/{served_model_name}/raw_kv_cache"
-        self.preprocess_cache_path = f"/mnt/data3/xmy/fusionrag_tree_cache/{served_model_name}/preprocess_kv_cache"
+        if not os.path.exists(cache_path_root):
+            cache_path_root = "/mnt/data"
+        self.cache_path = f"{cache_path_root}/xmy/fusionrag_tree_cache/{served_model_name}/raw_kv_cache"
+        self.preprocess_cache_path = f"{cache_path_root}/xmy/fusionrag_tree_cache/{served_model_name}/preprocess_kv_cache"
         if os.environ.get("DEBUG", "0") != "0":
-            self.cache_path = f"/mnt/data3/xmy/fusionrag_tree_cache_DEBUG/{served_model_name}/raw_kv_cache"
-            self.preprocess_cache_path = f"/mnt/data3/xmy/fusionrag_tree_cache_DEBUG/{served_model_name}/preprocess_kv_cache"
+            self.cache_path = f"{cache_path_root}/xmy/fusionrag_tree_cache_DEBUG/{served_model_name}/raw_kv_cache"
+            self.preprocess_cache_path = f"{cache_path_root}/xmy/fusionrag_tree_cache_DEBUG/{served_model_name}/preprocess_kv_cache"
         os.makedirs(self.cache_path, exist_ok=True)
         os.makedirs(self.preprocess_cache_path, exist_ok=True)
 
